e74ba32dd8320430b5269351363715b2a7675583
mrkkrp <markkarpov@opmbx.org>
2017-08-11 11:48:20 +0700

    Update the “commentary” section in source code

--- hasky-stack.el
+++ hasky-stack.el
@@ -25,7 +25,17 @@
 
 ;;; Commentary:
 
-;; Interface to the Stack development tool.  Proper description pending.
+;; This is an Interface to the Stack Haskell development tool.  Bind just
+;; two commands, like this:
+;;
+;;     (global-set-key (kbd "<next> h e") #'hasky-stack-execute)
+;;     (global-set-key (kbd "<next> h i") #'hasky-stack-new)
+;;
+;; * `hasky-stack-execute' opens a popup with a collection of stack commands
+;;   you can run.  Many commands have their own popups like in Magit.
+;;
+;; * `hasky-stack-new' allows to create a new project in current directory
+;;   using a Stack template.
 
 ;;; Code:
 
