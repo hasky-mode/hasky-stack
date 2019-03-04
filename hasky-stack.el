905376bea1e088ed282b6c483ad5281ae1953970
mrkkrp <markkarpov92@gmail.com>
2019-03-04 23:46:51 +0100

    Add GHC 8.6.3

--- hasky-stack.el
+++ hasky-stack.el
@@ -119,7 +119,7 @@ being used to compose command line."
   :tag  "Completing Function"
   :type '(radio (function-item completing-read)))
 
-(defcustom hasky-stack-ghc-versions '("8.4.4" "8.2.2" "8.0.2" "7.10.3" "7.8.4")
+(defcustom hasky-stack-ghc-versions '("8.6.3" "8.4.4" "8.2.2" "8.0.2" "7.10.3" "7.8.4")
   "GHC versions to pick from (for commands like \"stack setup\")."
   :tag  "GHC versions"
   :type '(repeat (string :tag "Extension name")))
